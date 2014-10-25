f2ed597399280addedc2bb1035562f37a2ea0fb8
Eru Penkman <eru.penkman@gmail.com>
2014-10-25 11:29:58 +1000

    logging/quickly debugging a production issue

--- server.js
+++ server.js
@@ -73,8 +73,10 @@ io.on('connection', function(socket) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", 'Content-Type, Accept, Origin, Cookie');
+  res.header("Access-Control-Allow-Headers",
+    'Content-Type, Accept, Origin, Cookie, X-Requested-With,' +
+    'X-DevTools-Emulate-Network-Conditions-Client-Id, Access-Control-Request-Headers, Access-Control-Request-Method, Referer'
+  );
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
