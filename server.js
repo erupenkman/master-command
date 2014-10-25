a952dc5e3d840da0bacb8a436cea4105f6cbf2c2
Eru Penkman <eru.penkman@gmail.com>
2014-10-25 11:23:53 +1000

    logging/quickly debugging a production issue

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", 'Content-Type, Accept, Origin, Cookie');
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
